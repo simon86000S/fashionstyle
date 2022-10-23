1fbf297bc6f93ac5e686b127b5817958fd440645
simon <simondu06730@hotmail.fr>
2022-10-23 22:00:06 +0200

    deploy

--- src/components/Home/Row/Row.jsx
+++ src/components/Home/Row/Row.jsx
@@ -12,10 +12,10 @@ const Row = () => {
     <div id='#/' className="Wrapper">
       <div className="author"><p>A<span >udrain</span></p><p>S<span>imon</span></p></div>
       <div className="wrapper-top ">
-        <div className="img1"></div>
-        <div className="img2"></div>
-        <div className=" img3"></div>
-        <div className="img4">
+        <div className="img img1"></div>
+        <div className="img img2"></div>
+        <div className=" img img3"></div>
+        <div className="img img4">
           <video
             src={defi}
             type="video/mp4"
@@ -26,19 +26,19 @@ const Row = () => {
           ></video>{" "}
         </div>
 
-        <div className="img5"></div>
-        <div className="img6"></div>
-        <div className="img7"></div>
-        <div className="img8"></div>
-        <div className="img9"></div>
-        <div className="img10"></div>
-        <div className="img11"></div>
-        <div className="img12"></div>
-        <div className="img13"></div>
-        <div className="img14"></div>
-        <div className="img15"></div>
-        <div className="img16"></div>
-        <div className="img17">
+        <div className="img img5"></div>
+        <div className="img img6"></div>
+        <div className="img img7"></div>
+        <div className="img img8"></div>
+        <div className="img img9"></div>
+        <div className="img img10"></div>
+        <div className="img img11"></div>
+        <div className="img img12"></div>
+        <div className="img img13"></div>
+        <div className="img img14"></div>
+        <div className="img img15"></div>
+        <div className="img img16"></div>
+        <div className="img img17">
           <video
             src={model}
             type="video/mp4"
@@ -48,7 +48,7 @@ const Row = () => {
             loop
           ></video>{" "}
         </div>
-        <div className="img18">
+        <div className="img img18">
           <video
             src={run}
             type="video/mp4"
@@ -63,12 +63,12 @@ const Row = () => {
         <h1>AS STYLE FASHION</h1>
       </div>
       <div className="wrapper-bottom">
-        <div className=" img1"></div>
-        <div className="img2"></div>
-        <div className=" img3"></div>
+        <div className=" img img1"></div>
+        <div className="img img2"></div>
+        <div className=" img img3"></div>
 
-        <div className="img4"></div>
-        <div className="img5">
+        <div className="img img4"></div>
+        <div className="img img5">
           <video
             src={model2}
             type="video/mp4"
@@ -78,7 +78,7 @@ const Row = () => {
             loop
           ></video>{" "}
         </div>
-        <div className="img6">
+        <div className="img img6">
           {" "}
           <video
             src={video2}
@@ -89,20 +89,20 @@ const Row = () => {
             height="240"
           ></video>{" "}
         </div>
-        <div className="img7"></div>
-        <div className="img8"></div>
-        <div className="img9"></div>
-        <div className="img10"></div>
-        <div className="img11"></div>
-        <div className="img12"></div>
-        <div className="img13"></div>
-        <div className="img14"></div>
-        <div className="img15"></div>
-        <div className="img16"></div>
-        <div className="img17"></div>
+        <div className="img img7"></div>
+        <div className="img img8"></div>
+        <div className="img img9"></div>
+        <div className="img img10"></div>
+        <div className="img img11"></div>
+        <div className="img img12"></div>
+        <div className="img img13"></div>
+        <div className="img img14"></div>
+        <div className="img img15"></div>
+        <div className="img img16"></div>
+        <div className="img img17"></div>
 
-        <div className="img18"></div>
-        <div className="img19">
+        <div className="img img18"></div>
+        <div className="img img19">
           <video
             src={video}
             type="video/mp4"
